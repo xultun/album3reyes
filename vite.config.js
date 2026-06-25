d69dbb5ce91f83a448159a5ddee06517898ed53c
LuisTrujillEspi <95892442+xultun@users.noreply.github.com>
2026-06-25 17:14:14 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Cambia 'album-3-reyes' por el nombre exacto de tu repositorio en GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Si usas dominio propio, déjalo en '/'. Si usas usuario.github.io/repo, cámbialo a '/repo/'
+  base: '/album3reyes/',  // Si usas dominio propio, déjalo en '/'. Si usas usuario.github.io/repo, cámbialo a '/repo/'
 })
