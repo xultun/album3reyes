93df197902923f0aa404c3112420214433fe2070
LuisTrujillEspi <95892442+xultun@users.noreply.github.com>
2026-06-25 17:15:55 -0500

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Cambia 'album-3-reyes' por el nombre exacto de tu repositorio en GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/album3reyes/',  // Si usas dominio propio, déjalo en '/'. Si usas usuario.github.io/repo, cámbialo a '/repo/'
+  base: '/album3reyes/',
 })
